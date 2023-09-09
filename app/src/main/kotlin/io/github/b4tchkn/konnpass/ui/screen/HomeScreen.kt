8e61bd9d45f4753b0eb20240356eb45a4631996e
b4tchkn <baaaakkrad@gmail.com>
2023-09-09 21:01:42 +0900

    chore: run ktlintFormat

--- app/src/main/kotlin/io/github/b4tchkn/konnpass/ui/screen/HomeScreen.kt
+++ app/src/main/kotlin/io/github/b4tchkn/konnpass/ui/screen/HomeScreen.kt
@@ -2,13 +2,11 @@ package io.github.b4tchkn.konnpass.ui.screen
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
