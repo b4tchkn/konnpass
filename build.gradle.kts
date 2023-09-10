4401b0fae87cd5ef425378a4c2f9bf958a3b53e1
Kakeru Nakabachi <baaaakkrad@gmail.com>
2023-09-10 18:28:03 +0900

    refactor: version catalog (#28)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidGradlePlugin) apply false
-    alias(libs.plugins.kotlinPlugin) apply false
-    alias(libs.plugins.serializationPlugin) apply false
-    alias(libs.plugins.hiltAndroidGradlePlugin) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.plugin.serialization) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
 }
