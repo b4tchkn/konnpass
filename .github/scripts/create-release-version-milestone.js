5eab2c5fdbb0e8bdbcd51553589052da731a19fb
b4tchkn <kakeru.nakabachi@zozo.com>
2025-06-02 17:51:24 +0900

    fix: branch name

--- .github/scripts/create-release-version-milestone.js
+++ .github/scripts/create-release-version-milestone.js
@@ -1,8 +1,12 @@
 module.exports = async ({ github, context, core }) => {
-    const currentVersion = context.ref.split('/')[1];
-    const nextVersion = currentVersion.replace(/v(\d+)\.(\d+)\.(\d+)/, (_, major, minor, patch) => {
+    const currentVersion = context.payload.pull_request?.head?.ref?.split('/')[1];
+    const nextVersion = currentVersion?.replace(/v(\d+)\.(\d+)\.(\d+)/, (_, major, minor, patch) => {
         return `v${major}.${Number(minor) + 1}.${patch}`;
     });
+    if (!nextVersion) {
+        core.setFailed("Could not determine next version from the current branch name.");
+        return;
+    }
     await github.rest.issues.createMilestone({
         owner: context.repo.owner,
         repo: context.repo.repo,
