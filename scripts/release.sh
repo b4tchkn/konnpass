30a93cb0f11c2fa4fa88e7146591416939da0e6b
b4tchkn <kakeru.nakabachi@zozo.com>
2025-06-04 18:42:51 +0900

    chore: update release sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+IS_HOTFIX=false
+if [[ "$1" == "--hotfix" ]]; then
+  echo "🚨 Hotfix mode is enabled. Only patch version will be updated."
+  IS_HOTFIX=true
+fi
+
 if [[ -n $(git status --porcelain) ]]; then
-  echo "🚨 ローカルに未コミットの差分があります。コミットまたはスタッシュしてから再実行してください。"
+  echo "🚨 You have uncommitted changes locally. Please commit or stash them before trying again."
   exit 1
 fi
 
@@ -23,35 +29,39 @@ UPDATED_MAJOR_VERSION_NAME="$((MAJOR + 1)).0.0($NEW_VERSION_CODE)"
 UPDATED_MINOR_VERSION_NAME="$MAJOR.$((MINOR + 1)).0($NEW_VERSION_CODE)"
 UPDATED_PATCH_VERSION_NAME="$MAJOR.$MINOR.$((PATCH + 1))($NEW_VERSION_CODE)"
 
-# ユーザーにリリースするバージョンを選択させて、MAJOR、MINOR、PATCHを更新
-echo "⚡️ Please select the version to release:"
-select choice in $UPDATED_MAJOR_VERSION_NAME $UPDATED_MINOR_VERSION_NAME $UPDATED_PATCH_VERSION_NAME; do
-  case $choice in
-    $UPDATED_MAJOR_VERSION_NAME)
-      MAJOR=$((MAJOR + 1))
-      MINOR=0
-      PATCH=0
-      break
-      ;;
-    $UPDATED_MINOR_VERSION_NAME)
-      MINOR=$((MINOR + 1))
-      PATCH=0
-      break
-      ;;
-    $UPDATED_PATCH_VERSION_NAME)
-      PATCH=$((PATCH + 1))
-      break
-      ;;
-    *)
-      echo "Please select a number between 1 and 3."
-      ;;
-  esac
-done
+if $IS_HOTFIX; then
+  # hotfix時はパッチバージョンだけを更新
+  PATCH=$((PATCH + 1))
+else
+  # ユーザーにリリースするバージョンを選択させて、MAJOR、MINOR、PATCHを更新
+  echo "⚡️ Please select the version to release:"
+  select choice in $UPDATED_MAJOR_VERSION_NAME $UPDATED_MINOR_VERSION_NAME $UPDATED_PATCH_VERSION_NAME; do
+    case $choice in
+      $UPDATED_MAJOR_VERSION_NAME)
+        MAJOR=$((MAJOR + 1))
+        MINOR=0
+        PATCH=0
+        break
+        ;;
+      $UPDATED_MINOR_VERSION_NAME)
+        MINOR=$((MINOR + 1))
+        PATCH=0
+        break
+        ;;
+      $UPDATED_PATCH_VERSION_NAME)
+        PATCH=$((PATCH + 1))
+        break
+        ;;
+      *)
+        echo "Please select a number between 1 and 3."
+        ;;
+    esac
+  done
+fi
 
 NEW_VERSION_NAME="$MAJOR.$MINOR.$PATCH"
 echo "🚀 Updating version to $NEW_VERSION_NAME($NEW_VERSION_CODE)"
-read -p "Do you want to release with this version? (y/n) [y]: " confirm
-confirm=${confirm:-y}
+read -p "Do you want to release with this version? (y/n): " confirm
 if [[ "$confirm" != "y" ]]; then
   echo "🚫 Release preparation has been cancelled."
   exit 1
@@ -63,15 +73,24 @@ if ! command -v gh >/dev/null 2>&1; then
   exit 1
 fi
 
-## developにcheckoutして最新にする
-git checkout develop
-git pull origin develop
 
-## sedコマンドを使ってversionCodeとversionNameを更新
+if $IS_HOTFIX; then
+  # 直近のタグを取得してcheckoutする
+  git fetch --tags
+  LATEST_TAG=$(git describe --tags --abbrev=0)
+  git checkout "$LATEST_TAG"
+else
+  # developにcheckoutして最新にする
+  git checkout develop
+  git pull origin develop
+fi
+
+
+# sedコマンドを使ってversionCodeとversionNameを更新
 sed -i '' "s/^versionCode = \".*\"/versionCode = \"$NEW_VERSION_CODE\"/" "$VERSION_TOML_FILE"
 sed -i '' "s/^versionName = \".*\"/versionName = \"$NEW_VERSION_NAME\"/" "$VERSION_TOML_FILE"
 
-## git commitしてdevelop->mainにPull Requestを作成
+# git commitしてmainにPull Requestを作成
 BRANCH_NAME="release/v$NEW_VERSION_NAME"
 git checkout -b "$BRANCH_NAME"
 git add "$VERSION_TOML_FILE"
